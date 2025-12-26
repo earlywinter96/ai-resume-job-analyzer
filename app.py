7c1cffb19efd7119fcfa0280f05cd67c7927d6e1
Hemant Solanki <HemantSolanki@Yogeshs-MacBook-Air.local>
2025-12-27 00:37:56 +0530

    Add Render deployment config

--- app.py
+++ app.py
@@ -84,6 +84,9 @@ def results():
 # -------------------------
 # App runner
 # -------------------------
-if __name__ == "__main__":
+#if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
